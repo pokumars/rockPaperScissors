6b93de7add86a44c3900366253a7505eba6e2ce8
Oheneba Poku-Marboah <ohenebakob@hotmail.com>
2019-04-12 16:16:02 +0300

    Initial Commit

---
+++ rockPaperScissors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rockPaperScissors
+//
+//  Created by iosdev on 12/04/2019.
+//  Copyright © 2019 oheneba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
