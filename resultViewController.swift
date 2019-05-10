4d1c576bd2b5b2dcd7acbdba06eb9742a17f1239
Oheneba Poku-Marboah <ohenebakob@hotmail.com>
2019-05-10 11:03:18 +0300

    first commit

---
+++ resultViewController.swift
@@ -0,0 +1,30 @@
+//
+//  resultViewController.swift
+//  rockPaperScissors
+//
+//  Created by iosdev on 12/04/2019.
+//  Copyright © 2019 oheneba. All rights reserved.
+//
+
+import UIKit
+
+class resultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ rockPaperScissors/resultViewController.swift
@@ -0,0 +1,9 @@
+//
+//  resultViewController.swift
+//  rockPaperScissors
+//
+//  Created by iosdev on 12/04/2019.
+//  Copyright © 2019 oheneba. All rights reserved.
+//
+
+import Foundation
